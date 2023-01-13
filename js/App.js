d96d5a0a7380ce34957cad3c244202991ab95b20
Fedir Melnyk <61160727+Phant24@users.noreply.github.com>
2023-01-13 09:37:08 +0200

    Було створенно можливість очищувати полотно та додана можливість на створення декількох ліній

--- js/App.js
+++ js/App.js
@@ -1,65 +1,92 @@
 let canvas = document.getElementById("canvas");
 canvas.height = 600;
 canvas.width = 800;
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext("2d");
 
 function drawLine(ctx, line) {
   const {
     start,
     end,
     lineWidth = 10,
-    lineCap = 'round', 
-    strokeStyle = 'black',
-  } = line
+    lineCap = "round",
+    strokeStyle = "black",
+  } = line;
 
-  if(!start || !end) {
-    throw new Error('Start or end of line not defined.')
-  }
-
-  ctx.beginPath()
-  ctx.moveTo(start.x, start.y)
-  ctx.lineTo(end.x, end.y)
-  ctx.lineWidth = lineWidth
-  ctx.lineCap = lineCap
-  ctx.strokeStyle = strokeStyle
-  ctx.stroke()
+  ctx.beginPath();
+  ctx.moveTo(start.x, start.y);
+  ctx.lineTo(end.x, end.y);
+  ctx.lineWidth = lineWidth;
+  ctx.lineCap = lineCap;
+  ctx.strokeStyle = strokeStyle;
+  ctx.stroke();
 }
 
-let isPressed = false;
-let mouseDownPos = null
+const mouse = {
+  isPressed: false,
+  down: null,
+  current: null,
+  up: null,
+  setDown: function (event, element) {
+    this.isPressed = true;
+    this.down = this.getPosition(event, element);
+  },
+  setUp: function (event, element) {
+    this.isPressed = false;
+    this.up = this.getPosition(event, element);
+  },
+  setCurrent: function (event, element) {
+    this.current = this.getPosition(event, element);
+  },
+  getPosition: function (event, element) {
+    let position = {
+      x: event.clientX - element.offsetLeft,
+      y: event.clientY - element.offsetTop,
+    };
+    return position;
+  },
+};
 
-document.addEventListener('mousedown', function(e) {
-  isPressed = true
-  mouseDownPos = {
-    x: e.clientX - canvas.offsetLeft,
-    y: e.clientY - canvas.offsetTop
-  }
+const lines = [];
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lines.forEach(function (line) {
+    drawLine(ctx, line);
+  });
+}
+
+function handleMouseDown(e) {
+  mouse.setDown(e, canvas);
 
   const line = {
-    start: mouseDownPos,
-    end: mouseDownPos,
-  }
+    start: mouse.down,
+    end: mouse.down,
+  };
+  lines.push(line);
 
-  drawLine(ctx, line)
-})
-
-document.addEventListener('mouseup', function() {
-  isPressed = false
-})
-
-document.addEventListener('mousemove', function(e) {
-  if(isPressed) {
-    let currentPos = {
-      x: e.clientX - canvas.offsetLeft,
-      y: e.clientY - canvas.offsetTop
-    }
+  draw();
+}
+function handleMuseUp(e) {
+  mouse.setUp(e, canvas);
+}
+function handleMouseMove(e) {
+  if (mouse.isPressed) {
+    mouse.setCurrent(e, canvas);
 
     let line = {
-      start: mouseDownPos,
-      end: currentPos
-    }
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-    drawLine(ctx, line)
+      start: mouse.down,
+      end: mouse.current,
+    };
+    lines.pop();
+    lines.push(line);
+    draw();
   }
-})
+}
+
+document.addEventListener("mousedown", handleMouseDown);
+document.addEventListener("mouseup", handleMuseUp);
+document.addEventListener("mousemove", handleMouseMove);
+
+document.getElementById("canvas_clear").addEventListener("click", function () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
